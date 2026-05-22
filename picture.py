7530386cea6e93dfc6d0575e76c710794453821a
thnguyen29 <thnguyen29@ssis.edu.vn>
2026-05-22 14:15:42 +0700

    Update picture.py

--- picture.py
+++ picture.py
@@ -33,8 +33,11 @@ def draw_picture(width, height):
    
    
     # code for GREEN triangles
+    sg.set_fill_color(colors[1])
     for i in range(5):   # fill_triangle(x1, y1, x2, y2, x3, y3)
-        sg.fill_triangle(2*TRIANGLE_WIDTH, i*TRIANGLE_HEIGHT, 3*TRIANGLE_WIDTH, i*TRIANGLE_HEIGHT, 3*TRIANGLE_WIDTH, (i+1)*TRIANG
+        sg.fill_triangle(TRIANGLE_WIDTH, i*TRIANGLE_HEIGHT, TRIANGLE_WIDTH, (i+1)*TRIANGLE_HEIGHT, 2*TRIANGLE_WIDTH, (i+1)*TRIANGLE_HEIGHT) 
+        
+
  
     # code for MAGENTA triangles
     
@@ -47,3 +50,4 @@ def draw_picture(width, height):
 if __name__ == "__main__":
     # Launch the wrapper; only edit starting dimensions of canvas if you would like to
     sg.start(draw_picture, 600, 400)
+
